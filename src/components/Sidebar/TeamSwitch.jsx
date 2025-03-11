30ccfc1b911323393bb44cdde5df03fb5921766a
Anthony Delmotte <anthony-delmotte@hotmail.fr>
2025-03-11 16:25:46 +0100

    ajout d'un lien de retour vers l'accueil en cliquant sur le logo

--- src/components/Sidebar/TeamSwitch.jsx
+++ src/components/Sidebar/TeamSwitch.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { Link } from "react-router-dom";
 
 export function TeamSwitcher({ teams }) {
   const [activeTeam] = React.useState(teams[0]);
@@ -7,19 +8,21 @@ export function TeamSwitcher({ teams }) {
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <SidebarMenuButton
-          size="lg"
-          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-        >
-          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-            <activeTeam.logo className="size-4" />
-          </div>
-          <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-semibold">
-              {activeTeam.name}
-            </span>
-          </div>
-        </SidebarMenuButton>
+        <Link to="/accueil" className="w-full">
+          <SidebarMenuButton
+            size="lg"
+            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          >
+            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+              <activeTeam.logo className="size-4" />
+            </div>
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate font-semibold">
+                {activeTeam.name}
+              </span>
+            </div>
+          </SidebarMenuButton>
+        </Link>
       </SidebarMenuItem>
     </SidebarMenu>
   );
